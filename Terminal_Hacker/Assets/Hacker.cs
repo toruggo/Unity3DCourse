d50bc9fb614f33cc837498db8c3a3a066deda0bc
toruggo <vhugo08.couto@gmail.com>
2020-08-29 17:58:03 -0300

    Conditional Program Flow Using If and Else

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
-	void Start () {
-		ShowMainMenu("Hugo");
+	void Start ()
+	{
+		ShowMainMenu();
 	}
 
-	void ShowMainMenu(string greetingName) {
+	void ShowMainMenu()
+	{
 		Terminal.ClearScreen();
-		Terminal.WriteLine("Hello " + greetingName + "!");
 		Terminal.WriteLine("What would you like to hack into?\n");
 		Terminal.WriteLine("Press 1 for the local library");
 		Terminal.WriteLine("Press 2 for the police station");
@@ -18,7 +19,31 @@ public class Hacker : MonoBehaviour {
 		Terminal.WriteLine("Enter your selection: ");
 	}
 
-	void OnUserInput(string input) {
-		print(input == "1");
+	void OnUserInput(string input)
+	{
+		if(input == "menu")
+		{
+			ShowMainMenu();
+		}
+		if(input == "1")
+		{
+			print("You chose the library");
+		}
+		else if(input == "2")
+		{
+			print("You chose the police station");
+		}
+		else if(input == "3")
+		{
+			print("You chose NASA");
+		}
+		else if(input == "007")
+		{
+			print("Please select a level, Mr Bond!");
+		}
+		else
+		{
+			print("There's no level " + input + ". Please chose a valid level!");
+		}
 	}
 }
