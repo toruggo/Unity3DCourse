2159141e2297eca96155e2173cde8a37d1226313
toruggo <vhugo08.couto@gmail.com>
2020-09-03 14:01:01 -0300

    Added Thrust Sound

--- Project_Booster/Assets/Rocket.cs
+++ Project_Booster/Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidBody;
+	AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,14 @@ public class Rocket : MonoBehaviour {
 		if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
 		{
 			rigidBody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
+		}
+		else
+		{
+			audioSource.Stop();
 		}
 		if (Input.GetKey(KeyCode.A))
 		{
