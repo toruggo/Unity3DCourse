8902c056c08baeb3c81f3ad799a65fd32c8b196f
toruggo <vhugo08.couto@gmail.com>
2020-09-04 17:36:36 -0300

    Moving Cycles

--- Project_Booster/Assets/Rocket.cs
+++ Project_Booster/Assets/Rocket.cs
@@ -94,7 +94,7 @@ public class Rocket : MonoBehaviour
 
 	private void ApplyThrust()
 	{
-		rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+		rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
 		if (!audioSource.isPlaying)
 		{
 			audioSource.PlayOneShot(mainEngine);
