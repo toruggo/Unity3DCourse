855515ba9551f5648caf983f8cee3883877ddd27
toruggo <vhugo08.couto@gmail.com>
2020-09-04 17:05:48 -0300

    Particles

--- Project_Booster/Assets/Rocket.cs
+++ Project_Booster/Assets/Rocket.cs
@@ -1,29 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Rocket : MonoBehaviour {
-
-	// todo finish lighting bug
+public class Rocket : MonoBehaviour
+{
 	[SerializeField] float rcsThrust = 100f;
 	[SerializeField] float mainThrust = 100f;
 	[SerializeField] AudioClip mainEngine;
 	[SerializeField] AudioClip success;
 	[SerializeField] AudioClip death;
 
+	[SerializeField] ParticleSystem mainEngineParticles;
+	[SerializeField] ParticleSystem successParticles;
+	[SerializeField] ParticleSystem deathParticles;
+
 	Rigidbody rigidBody;
 	AudioSource audioSource;
 	enum State { Alive, Dying, Transcending }
 	State state = State.Alive;
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		rigidBody = GetComponent<Rigidbody>();
 		audioSource = GetComponent<AudioSource>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		// somewhere stop sound on death
+	void Update ()
+	{
 		if (state == State.Alive)
 		{
 			RespondToThrustInput();
@@ -54,6 +56,7 @@ public class Rocket : MonoBehaviour {
 		state = State.Transcending;
 		audioSource.Stop();
 		audioSource.PlayOneShot(success);
+		successParticles.Play();
 		Invoke("LoadNextLevel", 1f);
 	}
 
@@ -62,6 +65,7 @@ public class Rocket : MonoBehaviour {
 		state = State.Dying;
 		audioSource.Stop();
 		audioSource.PlayOneShot(death);
+		deathParticles.Play();
 		Invoke("LoadFirstLevel", 1f);
 	}
 
@@ -84,6 +88,7 @@ public class Rocket : MonoBehaviour {
 		else
 		{
 			audioSource.Stop();
+			mainEngineParticles.Stop();
 		}
 	}
 
@@ -94,6 +99,7 @@ public class Rocket : MonoBehaviour {
 		{
 			audioSource.PlayOneShot(mainEngine);
 		}
+		mainEngineParticles.Play();
 	}
 
 	private void RespondToRotateInput()
